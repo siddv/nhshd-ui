b759a8335dfa9039b987b08d47531402f540e319
hclasen7 <51852596+hclasen7@users.noreply.github.com>
2019-06-16 09:12:57 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is a web page developed for NHS clinicians. The NHS website team cannot help with medical problems. </br> Urgent advice: </br>
+    <h1> </br>This is a web page developed for NHS clinicians. The NHS website team cannot help with medical problems. </br> </br> Urgent advice: </br> </br>
         If you think it's a life-threatening emergency: call 999</h1>
   </div>
 </template>
